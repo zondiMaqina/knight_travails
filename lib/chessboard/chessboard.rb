012ff1bf3c6f8da717bb614af0c28714534e2e6f
zondimaqina <zondimaqina0@gmail.com>
2024-11-26 22:08:52 +0200

    remove method for adding nodes in chessboard

--- lib/chessboard/chessboard.rb
+++ lib/chessboard/chessboard.rb
@@ -1,18 +1,9 @@
 require_relative '../vertices_set/nodes'
 # class for 2D array of board both physical for printing (chessboard) and digital (board)
 class ChessBoard
-  attr_accessor :board, :chessboard
+  attr_accessor :board
 
   def initialize
-    @board = board
-  end
-
-  def board
-    array = Array.new(8) {[]}
-    array.each do |row|
-     8.times do
-      row << Node.new
-     end
-    end
+    @board = []
   end
 end
