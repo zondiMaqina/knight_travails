479d1d846f3d6efce2ac777b7f80da8cadd66fa8
zondimaqina <zondimaqina0@gmail.com>
2024-11-26 16:12:49 +0200

    class for creating chessboard (2D array)

---
+++ lib/chessboard/chessboard.rb
@@ -0,0 +1,17 @@
+require_relative '../vertices_set/nodes'
+# class for 2D array of board both physical for printing (chessboard) and digital (board)
+class ChessBoard
+  attr_accessor :board, :chessboard
+
+  def initialize
+    @board = board
+  end
+
+  def board
+    array = Array.new(8) {[]}
+    array.each do |row|
+     8.times do
+      row << Node.new
+     end
+    end
+  end
--- lib/chessboard/knight_piece.rb
+++
@@ -1,3 +0,0 @@
-require_relative 'vertices_set/item'
-require_relative 'vertices_set/nodes'
-require_relative 'chessboard/chessboard'
