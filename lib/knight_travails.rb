426521ac9c7d7b3bf60e3b0069db912b6dcf717e
zondimaqina <zondimaqina0@gmail.com>
2024-11-26 16:09:51 +0200

    method for generating all valid moves on each coordinate (edges)

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -7,4 +7,20 @@ class KnightTravails
     @path_cost = {} # keeps track of paths in traversal
     @queue = [] # keeps track of all valid knight moves
   end
+
+  def edges(coordinates)
+    x = coordinates[0]
+    y = coordinates[1]
+    maximum_possible_displacements = [[-2, -1], [-2, 1], [2, -1], [2, 1], [1, -2], [-1, -2], [-1, 2], [1, 2]]
+    possible_moves = []
+    maximum_possible_displacements.each do |move|
+      possible_moves << [x + move[0], y + move[1]] if appropriate(move, x, y)
+    end
+    possible_moves # returns all possible moves from gievn point
+  end
+
+  def appropriate(move, index1, index2)
+    # method checks if the maximum possible moves apply to the gievn point
+    (index1 + move[0] >= 0 && index1 + move[0] < 8) && (index2 + move[1] >= 0 && index2 + move[1] < 8)
+  end
 end
