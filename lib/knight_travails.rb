bd8aaeee6ef65ab0874d84ad4cf90b78a2e7702d
zondimaqina <zondimaqina0@gmail.com>
2024-11-26 16:08:28 +0200

    class for knight travails

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -1,3 +1,10 @@
 require_relative 'vertices_set/item'
 require_relative 'vertices_set/nodes'
 require_relative 'chessboard/chessboard'
+
+class KnightTravails
+  def initialize
+    @path_cost = {} # keeps track of paths in traversal
+    @queue = [] # keeps track of all valid knight moves
+  end
+end
