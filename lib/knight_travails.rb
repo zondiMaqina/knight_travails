07620987f4410fd90f080458a12566611ed2fe83
zondimaqina <zondimaqina0@gmail.com>
2024-11-26 18:22:42 +0200

    loop that returns hash containing all possible moves counted by BFS from start

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -2,11 +2,11 @@ require_relative 'vertices_set/item'
 require_relative 'vertices_set/node'
 
 class KnightTravails
-  attr_accessor :start, :end
+  attr_accessor :start, :end, :path_cost
   def initialize
     @start = [0, 0] # default start
     @end = nil # default end
-    @path_cost = {@start: ['', '']} # keeps track of paths in traversal
+    @path_cost = {@start => ['', '']} # keeps track of paths in traversal
     @queue = [] # keeps track of all valid knight moves
   end
 
@@ -26,9 +26,27 @@ class KnightTravails
     (index1 + move[0] >= 0 && index1 + move[0] < 8) && (index2 + move[1] >= 0 && index2 + move[1] < 8)
   end
 
-  def knight_moves(start, end)
+  def knight_moves(start, endpoint)
+    cost = 0
+    set_move(start)
+    valid_moves = []
+   while !@queue.empty? || @path_cost[endpoint] != nil
+    cost += 1
+    current_move = @queue.shift
+    valid_moves = edges(current_move.to)
+    valid_moves.each {|move| @queue << Item.new(current_move.to, move, cost)} # adds next possible moves on queue
+    valid_moves.each {|move| @path_cost[move] = [current_move.to, cost] if @path_cost[move] == nil}
+    valid_moves = []
+    break if @path_cost[endpoint] != nil
+   end
+   @path_cost
+  end
+
+  def set_move(start)
+    @queue << Item.new(nil, start, 0)
+    @path_cost[@start] = [@queue[0].prev, @queue[0].cost]
   end
 end
 
 travails = KnightTravails.new
-p travails.edges([0, 0])
+p travails.knight_moves([0, 0], [3, 3])
