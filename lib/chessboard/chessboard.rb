c9030caaeaea1ac9654ce10e6ea813370e849d3c
zondimaqina <zondimaqina0@gmail.com>
2024-11-27 15:28:16 +0200

    fix if statement

--- lib/chessboard/chessboard.rb
+++ lib/chessboard/chessboard.rb
@@ -1,24 +1,24 @@
+require_relative 'knight_piece'
 require 'colorize'
 # class for 2D array of board both physical for printing (chessboard) and digital (board)
 class ChessBoard
   attr_accessor :board
-  
-  def initialize
-    @board = print_chessboard
-  end
 
-  def print_chessboard
-    (0..7).each do |row|
-      (0..7).each do |col|
-        if (row + col).even?
-          print "   ".on_black # Black square
-        else
-          print "   ".on_white # White square
+  def print_chessboard(path)
+    path.each do |move|
+      (0..7).each do |row|
+        (0..7).each do |col|
+          if (row == move[0] && col ==  move[0])
+            print "   ".on_green
+          elsif (row + col).even?
+            print "   ".on_black # Black square
+          else
+            print "   ".on_white # White square
+          end
         end
+        puts
       end
       puts
     end
   end
 end
-
-p ChessBoard.new.print_chessboard
