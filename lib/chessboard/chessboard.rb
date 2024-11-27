c236685b370f753939f9d5a4d2e1e01cf5820fb2
zondimaqina <zondimaqina0@gmail.com>
2024-11-27 15:04:45 +0200

    class for printing board

--- lib/chessboard/chessboard.rb
+++ lib/chessboard/chessboard.rb
@@ -1,9 +1,24 @@
-require_relative '../vertices_set/nodes'
+require 'colorize'
 # class for 2D array of board both physical for printing (chessboard) and digital (board)
 class ChessBoard
   attr_accessor :board
-
+  
   def initialize
-    @board = []
+    @board = print_chessboard
+  end
+
+  def print_chessboard
+    (0..7).each do |row|
+      (0..7).each do |col|
+        if (row + col).even?
+          print "   ".on_black # Black square
+        else
+          print "   ".on_white # White square
+        end
+      end
+      puts
+    end
   end
 end
+
+p ChessBoard.new.print_chessboard
