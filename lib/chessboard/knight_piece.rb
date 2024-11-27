653ca1491cac189abbd63f8077bfdaa6b63bb7d5
zondimaqina <zondimaqina0@gmail.com>
2024-11-27 14:46:39 +0200

    remove method for printing path

--- lib/chessboard/knight_piece.rb
+++ lib/chessboard/knight_piece.rb
@@ -7,15 +7,4 @@ class Knight
     @knight = '♞'.colorize(:green)
     @path = path
   end
-
-  def show_path
-    result = @path
-    if result.is_a?(Array)
-      string = "Nice, you made it in #{result.size - 1} moves "
-      result.each do |move|
-        string += move.to_s
-      end
-    end
-    string
-  end
 end
